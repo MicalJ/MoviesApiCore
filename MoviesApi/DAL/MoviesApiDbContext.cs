e31361f7d029a88ba171c1bf1794b1336602c5e8
Micał Jakub <mical.jakub@gmail.com>
2017-12-06 17:07:17 +0100

    Dodanie klas Movie, Category, Review i DbContext do folderu DAL

---
+++ MoviesApi/DAL/MoviesApiDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesApi.DAL
+{
+    public class MoviesApiDbContext : DbContext
+    {
+        public MoviesApiDbContext() : base()
+        { }
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+    }
+}
---
+++ MoviesApi/DAL/Review.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesApi.DAL
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public short Rate { get; set; }
+        public string Comment { get; set; }
+
+        public Movie Movie { get; set; }
+    }
+}
